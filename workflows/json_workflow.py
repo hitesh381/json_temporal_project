92481ae21421534fffeff756d4d166da9e5f831f
hitesh381 <34523421+hitesh381@users.noreply.github.com>
2025-07-21 20:00:32 +0530

    Update json_workflow.py

--- workflows/json_workflow.py
+++ workflows/json_workflow.py
@@ -1,9 +1,9 @@
 from temporalio import workflow
 import json
 
-
 @workflow.defn
 class JsonWorkflow:
     @workflow.run
-    async def run(self, data: dict) -> str:
+    async def run(self, data: list) -> str:
+        import json
         return json.dumps(data, indent=4)
