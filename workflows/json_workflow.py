fcb87e2cc591f6dca5e3c727ae43f14bae1dca0f
hitesh381 <34523421+hitesh381@users.noreply.github.com>
2025-07-21 19:51:12 +0530

    Update json_workflow.py

--- workflows/json_workflow.py
+++ workflows/json_workflow.py
@@ -1,13 +1,9 @@
 from temporalio import workflow
 import json
 
+
 @workflow.defn
 class JsonWorkflow:
     @workflow.run
-    async def run(self, json_string: str) -> str:
-        try:
-            parsed = json.loads(json_string)
-            pretty = json.dumps(parsed, indent=4)
-            return pretty
-        except json.JSONDecodeError:
-            return "Invalid JSON provided"
+    async def run(self, data: dict) -> str:
+        return json.dumps(data, indent=4)
